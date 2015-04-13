a24ad264f9839a5e2a24e8916f1e1bf688275749
zoxst <jovanstankovic@Jovan-Stankovics-MacBook-Pro.local>
2015-04-13 20:26:29 +0200

    Setting the background color. How cool is that?

--- ControllingSource/MasterViewController.m
+++ ControllingSource/MasterViewController.m
@@ -27,6 +27,8 @@
 
     UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
     self.navigationItem.rightBarButtonItem = addButton;
+    
+    self.view.backgroundColor = [UIColor blueColor];
 }
 
 - (void)didReceiveMemoryWarning {
